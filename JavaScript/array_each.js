c5fc6e0e7e2e34facdc1d9b5fccc067393cec56a
Ivan Stroganov <knowhere@gmail.com>
2012-07-03 19:53:50 -0700

    semicolons

--- JavaScript/array_each.js
+++ JavaScript/array_each.js
@@ -1,14 +1,14 @@
 Array.prototype.each = function(callbyname){
-    var length = this.length
+    var length = this.length;
     for (var i=0; i<length; i++) {
         callbyname(this[i]);
-    };
+    }
 };
 
 say_hi = function(item) {
-    alert("hi "+item)
+    alert("hi "+item);
 };
 
-at_this_table = ['oleg', 'ivan', 'ryan', 'natasha']
+at_this_table = ['oleg', 'ivan', 'ryan', 'natasha'];
 
-at_this_table.each(say_hi)
+at_this_table.each(say_hi);
