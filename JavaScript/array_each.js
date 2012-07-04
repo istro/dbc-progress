171564c8025de558723f51698bd5661e159e25dd
Ivan Stroganov <knowhere@gmail.com>
2012-07-03 19:14:00 -0700

    playing with javascript: reimplementing Array.each method

---
+++ JavaScript/array_each.js
@@ -0,0 +1,14 @@
+Array.prototype.each = function(callbyname){
+    var length = this.length
+    for (var i=0; i<length; i++) {
+        callbyname(this[i]);
+    };
+};
+
+say_hi = function(item) {
+    alert("hi "+item)
+};
+
+at_this_table = ['oleg', 'ivan', 'ryan', 'natasha']
+
+at_this_table.each(say_hi)
